975f79976f32f76b3ee66fe2073cb5d14b5bd8d0
r0ps3c <15878019+r0ps3c@users.noreply.github.com>
2020-11-05 05:51:59 -0500

    Update travis-install.sh

--- tests/travis-install.sh
+++ tests/travis-install.sh
@@ -18,7 +18,7 @@ ls -d1 /usr/local/lib/*-linux-gnu | sudo tee /etc/ld.so.conf.d/usrlocal.conf
 sudo ldconfig
 
 pip install https://github.com/rogerbinns/apsw/releases/download/3.8.2-r1/apsw-3.8.2-r1.zip
-pip install https://github.com/r0ps3c/coveralls-python/archive/fix-rebuilds.zip
+pip install https://github.com/r0ps3c/coveralls-python/archive/fix-rebuilds2.zip
 
 
 # We're pinning most packages to specific versions to prevent the CI from failing when
