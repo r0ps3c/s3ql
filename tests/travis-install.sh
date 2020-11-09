a4fc2b7a882b0233fe4399d2760c4e7e2015bb34
r0ps3c <15878019+r0ps3c@users.noreply.github.com>
2020-11-09 15:38:54 -0500

    Update travis-install.sh

--- tests/travis-install.sh
+++ tests/travis-install.sh
@@ -20,7 +20,6 @@ sudo ldconfig
 pip install https://github.com/rogerbinns/apsw/releases/download/3.8.2-r1/apsw-3.8.2-r1.zip
 pip install https://github.com/r0ps3c/coveralls-python/archive/fix-rebuilds2.zip
 
-
 # We're pinning most packages to specific versions to prevent the CI from failing when
 # testing eg merge requests because some of those packages have started emitting
 # depreciation warnings or made backwards incompatible changes.
